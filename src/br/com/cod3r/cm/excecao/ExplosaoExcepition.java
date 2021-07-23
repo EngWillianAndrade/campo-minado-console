451a2c9873321a5e655a529d271554085389a4e0
EngWillianAndrade <64414755+EngWillianAndrade@users.noreply.github.com>
2021-07-23 14:21:43 -0300

    Add files via upload

---
+++ src/br/com/cod3r/cm/excecao/ExplosaoExcepition.java
@@ -0,0 +1,9 @@
+package br.com.cod3r.cm.excecao;
+
+public class ExplosaoExcepition extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
---
+++ src/br/com/cod3r/cm/excecao/SairExcepition.java
@@ -0,0 +1,8 @@
+package br.com.cod3r.cm.excecao;
+
+public class SairExcepition extends RuntimeException {
+
+	private static final long serialVersionUID = 1;
+
+	
+}
